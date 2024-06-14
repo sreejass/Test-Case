bdcbe3b51e151cfae2fc58cd80d043780941837e
Sreeja Sreekumar Sujatha <Ssreekumarsujat9500@conestogac.on.ca>
2024-06-14 02:23:17 -0400

    Initial commit

--- EcommerceApp/obj/Debug/net7.0/EcommerceApp.AssemblyInfo.cs
+++ EcommerceApp/obj/Debug/net7.0/EcommerceApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3321d63bf1c0c0b43ae154f1fecca182ac3ab645")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d16cfe0432cb47eacaec4b743427f3c0ee622df")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
